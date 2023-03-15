3d66e75cabc6a1beb47b351345a40ae9adee4c68
MAteusz Dorosz <126687548+MDorosz@users.noreply.github.com>
2023-03-15 13:31:49 +0000

    suma poteg

--- Main.java
+++ Main.java
@@ -12,9 +12,12 @@ class Main
         System.out.print("Podaj druga liczbe: ");
         int b = scanner.nextInt(); 
         
-        int suma = a + b;
+        int potega1 = (int)Math.pow(a,b);
+        int potega2 = (int)Math.pow(b,a);
+
+        int suma = potega1 + potega2;
         
-        System.out.println("Suma liczb " + a + " i " + b + " wynosi " + suma); 
+        System.out.println("Suma  poteg liczb " + a + " i " + b + " wynosi " + suma); 
     
   }
 }
